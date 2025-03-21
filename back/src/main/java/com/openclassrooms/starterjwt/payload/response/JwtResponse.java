e76a3ca09bfb8931c2cc5b2f40dab72af45cd84f
ilyas cherfi <ilyas.cherfi@cliking.co>
2025-03-21 12:25:26 +0100

    add @NoArgsConstructor to JwtResponse class

--- back/src/main/java/com/openclassrooms/starterjwt/payload/response/JwtResponse.java
+++ back/src/main/java/com/openclassrooms/starterjwt/payload/response/JwtResponse.java
@@ -1,10 +1,12 @@
 package com.openclassrooms.starterjwt.payload.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class JwtResponse {
   private String token;
   private String type = "Bearer";
