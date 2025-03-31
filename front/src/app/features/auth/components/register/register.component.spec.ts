04e0ca00c277e177f7fd4773acfa8b7327a13b6d
ilyas cherfi <ilyascherfi91@gmail.com>
2025-03-31 18:54:57 +0200

    add integration test for register component

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,6 +1,6 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,10 +9,84 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { of, throwError } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let router: Router;
+
+  const mockAuthService = {
+    register: jest.fn()
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegisterComponent],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService }],
+      imports: [
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule
+      ]
+    })
+      .compileComponents();
+
+    router = TestBed.inject(Router);
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should register when valid informations are submitted", () => {
+    let registerRequest = { email: 'test@test.com', firstName: "John", lastName: "Doe", password: 'password' };
+    component.form.setValue(registerRequest);
+    const navigateSpy = jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
+    mockAuthService.register = jest.fn().mockReturnValue(of({}));
+
+    component.submit();
+
+    expect(mockAuthService.register).toHaveBeenCalledWith(registerRequest);
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+    expect(component.onError).toBe(false);
+    mockAuthService.register.mockClear();
+  });
+
+  it("should not register when back return error", () => {
+    let registerRequest = { email: 'test@test.com', firstName: "John", lastName: "Doe", password: 'password' };
+    component.form.setValue(registerRequest);
+    const navigateSpy = jest.spyOn(router, 'navigate').mockImplementation(jest.fn());
+    mockAuthService.register = jest.fn(() => throwError(() => new Error));
+
+    component.submit();
+
+    expect(mockAuthService.register).toHaveBeenCalledWith(registerRequest);
+    expect(navigateSpy).not.toHaveBeenCalled();
+    expect(component.onError).toBe(true);
+    mockAuthService.register.mockClear();
+  });
+});
+
+/*--------------- INTEGRATION TESTS ------------------*/
+
+describe('RegisterComponentIntegration', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+
+  let registerRequest = { email: 'test@test.com', firstName: "John", lastName: "Doe", password: 'password' };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,7 +94,7 @@ describe('RegisterComponent', () => {
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
@@ -34,7 +108,32 @@ describe('RegisterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('register if request is valid', () => {
+    component.form.setValue(registerRequest);
+    const httpSpy = jest.spyOn(HttpClient.prototype as any, 'post').mockReturnValue(of({})); //mocking call to back
+    const navigateSpy = jest.spyOn(Router.prototype as any, 'navigate').mockImplementation();
+
+    component.submit();
+
+    expect(httpSpy).toHaveBeenCalledWith("api/auth/register", registerRequest);
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+    expect(component.onError).toBe(false);
+    httpSpy.mockRestore();
+    navigateSpy.mockRestore();
   });
+
+  it('give an error if Backend returns error', () => {
+    component.form.setValue(registerRequest);
+    const httpSpy = jest.spyOn(HttpClient.prototype as any, 'post').mockImplementation(jest.fn(() => throwError(() => new Error)));
+    const navigateSpy = jest.spyOn(Router.prototype as any, 'navigate').mockImplementation();
+
+    component.submit();
+
+    expect(httpSpy).toHaveBeenCalledWith("api/auth/register", registerRequest);
+    expect(navigateSpy).not.toHaveBeenCalled();
+    expect(component.onError).toBe(true);
+    httpSpy.mockRestore();
+    navigateSpy.mockClear();
+  });
+
 });
